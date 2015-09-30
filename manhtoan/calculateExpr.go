7ba344301ca46c7392d4f85022ddd8fe4dfb28d4
ManhToan <nmt0504@gmail.com>
2015-09-30 15:17:58 +0700

    Implement Lexer Almost Complete

---
+++ manhtoan/calculateExpr.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	//	"bufio"
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+const (
+	ILLEGAL_TOKEN = iota
+	EOF
+	INLIT
+	//FLOATLIT
+	_IDENT
+	_OPERATOR
+	PARENTHESIS
+)
+
+var IN_LIT = regexp.MustCompile("[0-9]+")
+
+var OPERATOR = regexp.MustCompile(`\+|-|/|\*`)
+
+var IDENT = regexp.MustCompile("[a-zA-Z0-9]+")
+
+var SEPARATOR = regexp.MustCompile(`(|)`)
+
+func isTOKEN(str string) int {
+	if IN_LIT.MatchString(str) {
+		return INLIT
+	} else if OPERATOR.MatchString(str) {
+		return _OPERATOR
+	} else if IDENT.MatchString(str) {
+		return _IDENT
+	} else if SEPARATOR.MatchString(str) {
+		return PARENTHESIS
+	} else {
+		return ILLEGAL_TOKEN
+	}
+}
+
+func isSingleNumber(str string) bool {
+	if str >= "0" && str <= "9" {
+		return true
+	}
+	return false
+}
+
+func main() {
+	var str string = "1 + 2 * 3 * ( 4 - 2 ) + Aad / B + C"
+	// fmt.Print("Please input expression: ")
+	// scanner := bufio.NewScanner(strings.NewReader(str))
+	// scanner.Scan()
+
+	// fmt.Println(str)
+
+	//i := 0
+	for i := 0; i < len(str); i++ {
+		var tmp string
+		count := 0
+
+		// if space character -> skip
+		flagSpace := strings.Compare(string(str[i]), " ")
+		if flagSpace == 0 {
+			continue
+		}
+
+		if isSingleNumber(string(str[i])) {
+			for j := i; j < len(str); j++ {
+				if !isSingleNumber(string(str[j])) {
+					break
+				}
+				tmp += string(str[j])
+				count++
+			}
+		} else {
+			tmp = string(str[i])
+		}
+
+		// Print token
+		switch isTOKEN(string(tmp)) {
+		case INLIT:
+			fmt.Println("INLIT", tmp)
+		case _OPERATOR:
+			fmt.Println("OPERATOR", tmp)
+		case PARENTHESIS:
+			fmt.Println("PARENTHESIS", tmp)
+		case _IDENT:
+			fmt.Println("IDENT", tmp)
+		default:
+			fmt.Println("ERROR TOKEN", tmp)
+		}
+
+		if count > 0 {
+			i += count - 1
+		}
+	}
+
+}
