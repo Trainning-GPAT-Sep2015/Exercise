339fd25efcbde399a49e78fb281a66c43452ad82
Khiem <ngockhiem27@gmail.com>
2015-11-14 11:09:02 +0700

    .

--- ngockhiem/src/blogrethink/blog.go
+++ ngockhiem/src/blogrethink/blog.go
@@ -18,9 +18,22 @@ func main() {
 	if err != nil {
 		log.Fatalln(err.Error())
 	}
-	authors, _ := querry.GetAllAuthor(session)
-	articles, _ := querry.GetAllArticle(session)
-	fmt.Println(authors)
-	fmt.Println(articles)
-	fmt.Println("DONE")
+	// authors, _ := querry.GetAllAuthor(session)
+	// articles, _ := querry.GetAllArticle(session)
+	// articlesfromtag, _ := querry.GetAllArticleByTag("the", session)
+	// fmt.Printf("%v", authors)
+	// fmt.Println(articles)
+	// fmt.Println(articlesfromtag)
+	// fmt.Println("DONE")
+
+	message, _ := querry.GetMessagesByRoom(session, 3)
+	for _, item := range message {
+		fmt.Printf("%v : %v \n", item.CreatedTime, item.Content)
+	}
+	cursor, _ := querry.SubscribeMessages(session, 3, func(err error, item *querry.Message) {
+		fmt.Printf("%v : %v \n", item.CreatedTime, item.Content)
+	})
+	ch := make(chan int)
+	<-ch
+	fmt.Println(cursor)
 }
--- ngockhiem/src/blogrethink/querry/querry.go
+++ ngockhiem/src/blogrethink/querry/querry.go
@@ -1,8 +1,8 @@
 package querry
 
 import (
-	"fmt"
 	r "github.com/dancannon/gorethink"
+	"time"
 )
 
 const (
@@ -13,109 +13,111 @@ const (
 )
 
 type Author struct {
-	name     string
-	id       float64
-	articles []float64
+	Name string `gorethink:"name"`
+	Id   string `gorethink:"id"`
 }
 
 type Article struct {
-	title   string
-	content string
-	author  float64
-	tags    []float64
-	id      float64
+	Title   string   `gorethink:"title"`
+	Content string   `gorethink:"content"`
+	Author  string   `gorethink:"author"`
+	Tag     []string `gorethink:"tag"`
+	Id      string   `gorethink:"id"`
 }
 
-type Tag struct {
-	name     string
-	articles []float64
-}
-
-func GetAllArticle(session *r.Session) ([]Article, error) {
-	res, err := r.DB(DB_NAME).Table(ARTICLE_TABLE).Run(session)
+func GetAllArticle(session *r.Session) ([]*Article, error) {
+	cursor, err := r.DB(DB_NAME).Table(ARTICLE_TABLE).Run(session)
 	if err != nil {
-		return []Article{}, err
+		return nil, err
 	}
-	defer res.Close()
-	var articles []Article
-	var row interface{}
-	for res.Next(&row) {
-		var article Article
-		if convert, ok := row.(map[string]interface{}); ok {
-			article.title = convert["title"].(string)
-			article.content = convert["content"].(string)
-			article.id = convert["id"].(float64)
-			if convert["author"] != nil {
-				article.author = convert["author"].(float64)
-			}
-			if convert["tags"] != nil {
-				tags := convert["tags"].([]interface{})
-				for _, tag := range tags {
-					article.tags = append(article.tags, tag.(float64))
-				}
-			}
-			articles = append(articles, article)
-		} else {
-			fmt.Println("Cannot convert article row")
-		}
-	}
+	var articles []*Article
+	cursor.All(&articles)
 	return articles, nil
 }
-func GetAllAuthor(session *r.Session) ([]Author, error) {
-	res, err := r.DB(DB_NAME).Table(AUTHOR_TABLE).Run(session)
+
+func GetAllAuthor(session *r.Session) ([]*Author, error) {
+	cursor, err := r.DB(DB_NAME).Table(AUTHOR_TABLE).Run(session)
 	if err != nil {
-		return []Author{}, err
+		return nil, err
 	}
-	defer res.Close()
-	var authors []Author
-	var row interface{}
-	for res.Next(&row) {
-		var author Author
-		if convert, ok := row.(map[string]interface{}); ok {
-			author.id = convert["id"].(float64)
-			author.name = convert["name"].(string)
-			if convert["articles"] != nil {
-				articles := convert["articles"].([]interface{})
-				for _, article := range articles {
-					author.articles = append(author.articles, article.(float64))
-				}
-			}
-			authors = append(authors, author)
-		} else {
-			fmt.Println("Cannot convert row")
-		}
-	}
-	res.All(&authors)
+	var authors []*Author
+	cursor.All(&authors)
 	return authors, nil
 }
 
-func GetAuthorArticle(Author author) ([]Article, error) {
-	res, err := r.DB(DB_NAME).Table(ARTICLE_TABLE).Run(session)
+func GetAllArticleByAuthor(author_id string, session *r.Session) ([]*Article, error) {
+	cursor, err := r.DB(DB_NAME).Table(ARTICLE_TABLE).GetAllByIndex("author_id", author_id).Run(session)
 	if err != nil {
-		return []Article{}, err
+		return nil, err
 	}
-	defer res.Close()
-	var articles []Article
-	var row interface{}
-	for res.Next(&row) {
-		var article Article
-		if convert, ok := row.(map[string]interface{}); ok {
-			article.title = convert["title"].(string)
-			article.content = convert["content"].(string)
-			article.id = convert["id"].(float64)
-			if convert["author"] != nil {
-				article.author = convert["author"].(float64)
-			}
-			if convert["tags"] != nil {
-				tags := convert["tags"].([]interface{})
-				for _, tag := range tags {
-					article.tags = append(article.tags, tag.(float64))
-				}
-			}
-			articles = append(articles, article)
-		} else {
-			fmt.Println("Cannot convert article row")
-		}
-	}
+	var articles []*Article
+	cursor.All(&articles)
 	return articles, nil
+}
+
+func GetAllArticleByTag(tag string, session *r.Session) ([]*Article, error) {
+	cursor, err := r.DB(DB_NAME).Table(ARTICLE_TABLE).GetAllByIndex("tag", tag).Run(session)
+	if err != nil {
+		return nil, err
+	}
+	var articles []*Article
+	cursor.All(&articles)
+	return articles, nil
+}
+
+type Message struct {
+	Content     string    `gorethink:"content"`
+	CreatedTime time.Time `gorethink:"created_time"`
+}
+
+func GetMessagesByRoom(session *r.Session, roomId int) (messages []*Message, err error) {
+	cursor, err := r.DB(DB_NAME).Table("message").
+		Between([]interface{}{roomId, r.MinVal}, []interface{}{roomId, r.MaxVal}, r.BetweenOpts{
+		Index: "room_time",
+	}).
+		OrderBy(r.OrderByOpts{Index: "room_time"}).
+		Limit(30).
+		Run(session)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = cursor.All(&messages)
+	return
+}
+
+func SubscribeMessages(session *r.Session, roomId int, callback func(err error, message *Message)) (*r.Cursor, error) {
+	cursor, err := r.DB(DB_NAME).Table("message").
+		Between([]interface{}{roomId, r.MinVal}, []interface{}{roomId, r.MaxVal}, r.BetweenOpts{
+		Index: "room_time",
+	}).
+		Changes().
+		Run(session)
+
+	if err != nil {
+		return nil, err
+	}
+
+	go func() {
+		var change struct {
+			NewVal *Message `gorethink:"new_val"`
+			OldVal *Message `gorethink:"old_val"`
+		}
+		for {
+			ok := cursor.Next(&change)
+			if !ok {
+				err := cursor.Err()
+				cursor.Close()
+				callback(err, nil)
+				return
+			}
+
+			if change.NewVal != nil {
+				callback(nil, change.NewVal)
+			}
+		}
+	}()
+
+	return cursor, nil
 }
